4030867d870c13619bcb9b77cb3aaa932b273cdb
tjayling <tjayling@yahoo.co.uk>
2022-11-18 14:30:53 +0000

    Styled match page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const header = () => {
   return (
     <div className="header-container">
-      <div className="logo-container">
-        <h1 className="logo-initials">tjayling.space</h1>
-      </div>
-      {/* <div className="title-container"><h1 className="title">tjayling.space</h1></div> */}
-      <div className="logo" />
+      <Link className="link" to="/">
+        <h1 className="title">tjayling.space</h1>
+      </Link>
     </div>
   );
 };
